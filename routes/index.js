7228359da914663cf6f47f7a2f4613f8241f8302
JoshRicks <josh.ricks17@gmail.com>
2025-05-25 23:33:02 -0400

    changes from code review

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const clothingRouter = require("./clothingItems");
 const userRouter = require("./users");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/items", clothingRouter);
 router.use("/users", userRouter);
 
 router.use((req, res) => {
   console.error();
-  res.status(404).send({ message: "Requested resource not found" });
+  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
