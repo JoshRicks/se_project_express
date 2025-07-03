1060717345b04350802ccb6f8636db732d509b7d
JoshRicks <josh.ricks17@gmail.com>
2025-07-03 13:31:02 -0400

    added crash test for code review

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(requestLogger);
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.use("/", routes);
 
 app.use(errorLogger);
